2fbd776eba3c603dad8f67993c4f94f32b869b49
tristan <tristan.porteries@gmail.com>
2021-04-24 16:32:51 +0200

    feat: send email to all guests of a tournament request

--- src/main/java/fr/univ_smb/isc/m1/brutocollinus/application/EmailService.java
+++ src/main/java/fr/univ_smb/isc/m1/brutocollinus/application/EmailService.java
@@ -1,9 +1,14 @@
 package fr.univ_smb.isc.m1.brutocollinus.application;
 
+import fr.univ_smb.isc.m1.brutocollinus.adapters.api.TournamentRequestController;
 import fr.univ_smb.isc.m1.brutocollinus.infrastructure.persistence.entity.TournamentRequest;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 public class EmailService {
@@ -11,19 +16,26 @@ public class EmailService {
 
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
+    }
 
+    private void sendTo(String content, String subject, String to) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
 
-        mailMessage.setTo("tristan.porteries@free.fr");
-        mailMessage.setSubject("Hello");
-        mailMessage.setText("Hello World");
-
-        mailMessage.setFrom("noreply@brutocollinus.oups.net");
+        mailMessage.setText(content);
+        mailMessage.setSubject(subject);
+        mailMessage.setTo(to);
 
         this.mailSender.send(mailMessage);
     }
 
     public void sendTournamentRequestByEmailToAllGuests(TournamentRequest request) {
+        String tournamentName = request.name();
+        WebMvcLinkBuilder acceptTournamentRequestLink = linkTo(methodOn(TournamentRequestController.class).accept(request.uuid(), null));
+        String acceptTournamentRequestHref = acceptTournamentRequestLink.toString();
 
+        String subject = String.format("Request on tournament \"%s\"", tournamentName);
+        String content = String.format("You have been invited to tournament \"%s\", go to this link to accept: %s", tournamentName, acceptTournamentRequestHref);
+
+        request.guests().forEach(guest -> this.sendTo(content, subject, guest.email()));
     }
 }
