c96482575279dae24e4144df5cf7198919ee724d
tristan <tristan.porteries@gmail.com>
2021-04-27 19:08:06 +0200

    chore: build frontend in spring jar

--- src/main/java/fr/univ_smb/isc/m1/brutocollinus/adapters/api/security/WebConfig.java
+++ src/main/java/fr/univ_smb/isc/m1/brutocollinus/adapters/api/security/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
+/*@Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
@@ -13,4 +13,4 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
-}
+}*/
